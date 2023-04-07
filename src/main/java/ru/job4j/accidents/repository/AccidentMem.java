2fa60953d570a473a929c9e43108c2ea6318caf9
AMkhitarants <avetis.mkhitaryants@gmail.com>
2023-04-07 07:03:08 +0300

    5. Form с агрегационными объектами [#305523]

--- src/main/java/ru/job4j/accidents/repository/AccidentMem.java
+++ src/main/java/ru/job4j/accidents/repository/AccidentMem.java
@@ -1,30 +1,19 @@
 package ru.job4j.accidents.repository;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
 import ru.job4j.accidents.model.Accident;
-import ru.job4j.accidents.model.AccidentType;
-import ru.job4j.accidents.model.Rule;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Repository
+@AllArgsConstructor
 public class AccidentMem implements Store {
 
     private final AtomicInteger currentId = new AtomicInteger(0);
     private final Map<Integer, Accident> accidents = new ConcurrentHashMap<>();
-
-    public AccidentMem() {
-        Set<Rule> rules = Set.of(
-                new Rule(1, "Статья. 1"),
-                new Rule(2, "Статья. 2"),
-                new Rule(3, "Статья. 3"));
-        Accident acc1 = new Accident(currentId.incrementAndGet(), "Сбил пешехода",
-                "Черный седан номера м138ае26", "Московская 50",
-                new AccidentType(1, "Две машины"), rules);
-        this.accidents.put(acc1.getId(), acc1);
-    }
 
     @Override
     public List<Accident> findAll() {
